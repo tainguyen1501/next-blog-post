e020705169bd2621778f9ae864df4e7daa3a2d87
tainguyen1501 <felix.nguyen@transwap.com>
2023-11-28 15:40:40 +0700

    upfaet

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,19 +1,35 @@
-import dynamic from 'next/dynamic'
-import { Suspense } from 'react'
-const EditorComp = dynamic(() => import('../../components/editor/EditorComponent'), { ssr: false })
+"use client";
+
+import utils from "@/utils/utils";
+import dynamic from "next/dynamic";
+import { Suspense, useState } from "react";
+const EditorComp = dynamic(
+  () => import("../../components/editor/EditorComponent"),
+  { ssr: false }
+);
 
 const markdown = `
 # Hello world!
 Check the EditorComponent.tsx file for the code .
-`
+`;
 export default function TestPage() {
+  const [slug, setSlug] = useState("");
+  const handleOnchangeText = (e: any) => {
+    console.log(e.target.value);
+    setSlug(utils.string.slugify(e.target.value));
+  };
   return (
-   <>
-   <div>Editor:
-      <Suspense fallback={null}>
-        <EditorComp markdown={markdown} />
-      </Suspense>
-    </div>
-   </>
-  )
+    <>
+      <div className="flex min-h-screen flex-col items-center">
+        {slug}
+        <input onChange={(e) => handleOnchangeText(e)} />
+        <div>
+          Editor:
+          <Suspense fallback={null}>
+            <EditorComp markdown={markdown} />
+          </Suspense>
+        </div>
+      </div>
+    </>
+  );
 }
---
+++ src/utils/utils.ts
@@ -0,0 +1,21 @@
+const utils = {
+  string: {
+    slugify: (input: string) => {
+      if (!input) return "";
+      // make lower case and trim
+      var slug = input.toLowerCase().trim();
+      slug = slug.replaceAll('đ', 'd')
+      // remove accents from charaters
+      slug = slug.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+
+      // replace invalid chars with spaces
+      slug = slug.replace(/[^a-z0-9\s-]/g, " ").trim();
+
+      // replace multiple spaces or hyphens with a single hyphen
+      slug = slug.replace(/[\s-]+/g, "-");
+
+      return slug;
+    },
+  },
+};
+export default utils;
